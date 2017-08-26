2ab72be4b3c9524b3aac6ddba824ef6283d297c0
Daniel <lahuman@daum.net>
2017-08-27 02:39:11 +0900

    word counter start

---
+++ src/main/java/lahuman/wordcnt/WordcntApplication.java
@@ -0,0 +1,12 @@
+package lahuman.wordcnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcntApplication.class, args);
+	}
+}
